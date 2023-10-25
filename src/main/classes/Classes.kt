3710ded357bee3e5be9ef05a3c4835b63378f85b
Maria Manakhova <msmanakhova@edu.hse.ru>
2023-10-26 00:23:48 +0400

    finished all tasks

--- src/main/classes/Classes.kt
+++ src/main/classes/Classes.kt
@@ -1,3 +1,13 @@
+fun employeeById(id: Int) = when (id) {
+    1 -> Employee("Mary", 20)
+    2 -> null
+    3 -> Employee("John", 21)
+    4 -> Employee("Ann", 23)
+    else -> null
+}
+
+fun salaryById(id: Int) = employeeById(id)?.salary ?: 0
+
 fun main() {
     val employee = Employee("Mary", 20)
     println(employee)
@@ -8,4 +18,8 @@ fun main() {
     println(employeeGenerator.generateEmployee())
     employeeGenerator.minimumSalary = 15
     println(employeeGenerator.generateEmployee())
+
+    println(salaryById(2))
+    println(salaryById(3))
+    println((1..5).sumOf { id -> salaryById(id) })
 }
